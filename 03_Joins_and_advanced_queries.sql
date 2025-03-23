c19a72c7758478dc7e26e177245ce30cd2fcea91
Sreelakshmi A Shyjumon <162820134+sreelakshmiasy@users.noreply.github.com>
2025-03-23 22:40:57 +0000

    Update 03_Joins_and_advanced_queries.sql

--- 03_Joins_and_advanced_queries.sql
+++ 03_Joins_and_advanced_queries.sql
@@ -18,6 +18,36 @@ ON bo.product_id = bp.product_id
 GROUP BY bp.product_id, bp.product_name, bp.category
 ORDER BY total_revenue DESC;
 
+#Total revenue is the highest for eggs
+#Total profit is the highest for Snacks
+
+
+#Brand profit
+SELECT bp.brand,
+SUM(bo.quantity) AS total_units_sold,
+ROUND(SUM(bo.quantity * bo.unit_price), 2) AS total_revenue,
+SUM(bo.quantity * (bp.margin_percentage/100) * bo.unit_price) AS  total_profit
+FROM blinkit_orders bo LEFT JOIN blinkit_products bp
+ON bo.product_id = bp.product_id
+GROUP BY bp.brand
+ORDER BY total_revenue DESC;
+
+#highest profit and revenue: Bahl-Pau, Chahal Group
+#profit and revenue: Pet Care, Diary & Breakfast
+
+#Total company revenue
+SELECT SUM(total_revenue) AS total_company_revenue
+FROM (SELECT bp.product_id, bp.product_name, bp.category,
+SUM(bo.quantity) AS total_units_sold,
+ROUND(SUM(bo.quantity * bo.unit_price), 2) AS total_revenue,
+SUM(bo.quantity * (bp.margin_percentage/100) * bo.unit_price) AS  total_profit
+FROM blinkit_orders bo LEFT JOIN blinkit_products bp
+ON bo.product_id = bp.product_id
+GROUP BY bp.product_id, bp.product_name, bp.category
+ORDER BY total_revenue DESC) AS revenue_table;
+
+#total company revenue is 1043018.22 INR
+
 #TOP SELLING PRODUCTS
 SELECT bp.product_name, bp.category,
 COUNT(bo.order_id) AS total_orders,
@@ -26,6 +56,9 @@ FROM blinkit_orders bo LEFT JOIN blinkit_products bp
 ON bo.product_id = bp.product_id
 GROUP BY bp.product_name, bp.category
 ORDER BY total_units_sold DESC;
+
+#The top selling products are Vitamins, Lotion, Pet treats and baby wipes
+
 
 #Leading brands
 
@@ -36,6 +69,9 @@ FROM blinkit_orders bo LEFT JOIN blinkit_products bp
 ON bo.product_id = bp.product_id
 GROUP BY bp.category, bp.brand
 ORDER BY total_units_sold DESC;
+
+#Highest number of units sold from brand Raja and Sons for category pet care
+
 
 #Insights on inventory management
 
@@ -58,6 +94,17 @@ ORDER BY stock_status;
 #Brands and price trends
 #Discount across brands
 
+SELECT AVG(avg_discount)
+FROM (SELECT bp.brand, 
+AVG(bo.unit_price) AS avg_selling_price,
+AVG(bp.mrp) AS avg_mrp,
+AVG(bp.mrp) - AVG(bo.unit_price) AS avg_discount
+FROM blinkit_orders bo LEFT JOIN blinkit_products bp
+ON bo.product_id = bp.product_id
+GROUP BY bp.brand
+ORDER BY avg_discount DESC) AS avg_disc;
+
+
 SELECT bp.brand, 
 AVG(bo.unit_price) AS avg_selling_price,
 AVG(bp.mrp) AS avg_mrp,
@@ -66,3 +113,15 @@ FROM blinkit_orders bo LEFT JOIN blinkit_products bp
 ON bo.product_id = bp.product_id
 GROUP BY bp.brand
 ORDER BY avg_discount DESC;
+#The highest avg discount is provided by the brand Mammen-Hegde
+
+SELECT bp.category, 
+AVG(bo.unit_price) AS avg_selling_price,
+AVG(bp.mrp) AS avg_mrp,
+AVG(bp.mrp) - AVG(bo.unit_price) AS avg_discount
+FROM blinkit_orders bo LEFT JOIN blinkit_products bp
+ON bo.product_id = bp.product_id
+GROUP BY bp.category
+ORDER BY avg_discount DESC;
+
+#Instant & Frozen food is the category with the highest average discount closely followed by Pet Care
